cc62abcc86fc3bf6c0780fd24c650387c6e6bbdb
Mario4009 <mario.voirol@gmail.com>
2023-11-26 21:56:34 +0100

    feat: added tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdvancedComponent } from './domain/advanced/advanced.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdvancedComponent
   ],
   imports: [
     BrowserModule,
