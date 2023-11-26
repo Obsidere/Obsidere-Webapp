680f4594ed30a4c3915ad0b8b2e30892099783b2
Mario4009 <mario.voirol@gmail.com>
2023-11-26 22:10:24 +0100

    feat: added routing for advanced-mode

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/ad', pathMatch: 'full' },
+  { path: 'ad', loadChildren: () => import('./domain/advanced/advanced.module').then((m) => m.AdvancedModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/domain/advanced/advanced-routing.module.ts
+++ src/app/domain/advanced/advanced-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AdLayoutComponent} from "./ad-layout/ad-layout.component";
+import {AdDashboardModule} from "./components/ad-dashboard/ad-dashboard.module";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AdLayoutComponent, children: [
+      { path: '', loadChildren: () => import('./components/ad-home/ad-home.module').then((m) => m.AdHomeModule) },
+      { path: 'dashboard', loadChildren: () => import('./components/ad-dashboard/ad-dashboard.module').then((m) => AdDashboardModule) },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
